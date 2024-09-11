0c69f74311d369930cb380b25282196442c3179c
Arya Emami <aryaemami59@yahoo.com>
2024-09-11 04:37:43 -0500

    Fix `rollup.config.ts`

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,76 +1,21 @@
 import commonjs from '@rollup/plugin-commonjs'
-import nodeResolve from '@rollup/plugin-node-resolve'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
 import replace from '@rollup/plugin-replace'
 import { defineConfig } from 'rollup'
 
 export default defineConfig({
   treeshake: true,
   experimentalLogSideEffects: true,
-  // preserveEntrySignatures: 'strict',
-  // external: ['immer', 'reselect'],
-  // external: Object.keys(dependencies)
-  //   .concat(Object.keys(peerDependencies))
-  //   .concat(['use-sync-external-store/with-selector.js']),
-  // external: [
-  //   'react',
-  //   'use-sync-external-store',
-  //   // 'react-dom',
-  //   'use-sync-external-store/with-selector.js',
-  //   'redux',
-  //   // 'immer',
-  //   // 'reselect',
-  // ],
   input: 'src/index.ts',
   output: [
     {
-      // dir: 'dist',
-      // preserveModules: true,
       file: 'dist/withRollup.js',
-      // format: 'module',
-      // interop: "esModule",
       format: 'esm',
-      // exports: 'named',
-      // validate: true,
-      // strict: true,
-      // interop: 'esModule'
-      // esModule: "if-default-prop",
-      // indent: true,
-      // strict: true,
-      // dynamicImportInCjs: true,
-
-      // esModule: "if-default-prop",
-      // interop: 'auto',
     },
-    // {
-    //   file: './dist/withRollupUglified.js',
-    //   format: 'esm',
-    //   plugins: [
-    //     terser({
-    //       mangle: false,
-    //       output: {
-    //         beautify: true,
-    //       },
-    //     }),
-    //   ],
-    // },
   ],
   plugins: [
-    // commonjs({
-    //   // extensions: ['.cjs'],
-    //   // requireReturnsDefault: "preferred",
-    //   // esmExternals: true,
-    //   // requireReturnsDefault: 'namespace',
-    //   // defaultIsModuleExports: false,
-    //   // esmExternals: true,
-    //   // strictRequires: true,
-    // }),
-    nodeResolve({
-      // extensions: ['.cjs'],
-      // exportConditions: ['require'],
-      // resolveOnly: ['reselect'],
-      // mainFields: ['main'],
-    }),
-    // nodeResolve({ exportConditions: ['require'], extensions: ['.cjs'] }),
+    commonjs({}),
+    nodeResolve({}),
     replace({
       'process.env.NODE_ENV': JSON.stringify('production'),
       preventAssignment: true,
